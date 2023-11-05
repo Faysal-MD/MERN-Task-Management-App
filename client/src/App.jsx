92eef64d86a901c6ea3660189619a2cd39cd5c0b
Faysal598 <faysalmahmud735@gmail.com>
2023-11-05 09:14:44 +0600

    header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,10 @@
-import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from "./components/authentication/Signup";
-import Signin from "./components/authentication/Signin";
-import Task from "./components/task/Task";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { authActions } from "./store";
+import Header from "./components/Header";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const id = sessionStorage.getItem("id");
-    if (id) {
-      dispatch(authActions.login());
-    }
-  }, []);
-
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/task" element={<Task />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/logout" element={<Home />} />
-        </Routes>
-      </Router>
-    </>
+    <div className="w-52 mx-auto">
+      <Header />
+    </div>
   );
 };
 
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <header>
+        <p className='my-20 text-5xl font-semibold text-gray-700'>
+            Task Lists
+        </p>
+    </header>
+  )
+}
+
+export default Header
